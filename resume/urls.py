24f5ef896ae5aba19cd587210902d001ce7ac13d
Ahmed Said <kilicsaud@gmail.com>
2024-03-07 18:30:16 +0300

    fix: link bug fixed

--- resume/urls.py
+++ resume/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls'))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
