8a5a33b18e3a7676d792432d58ae7002c6061c47
Ahmed Said <kilicsaud@gmail.com>
2024-03-14 03:13:18 +0300

    refactor: application of general setting

--- core/models.py
+++ core/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class GeneralSettings(models.Model):
+class GeneralSetting(models.Model):
     name = models.CharField(
         default='',
         max_length=254,
+        blank=True,
+        verbose_name='Name',
 
     )
 
@@ -13,17 +15,32 @@ class GeneralSettings(models.Model):
         default='',
         max_length=254,
         blank=True,
+        verbose_name='Description',
     )
     parameter = models.CharField(
         default='',
         max_length=254,
         blank=True,
+        verbose_name='Parameter',
+
     )
     update_date = models.DateTimeField(
         blank=True,
         auto_now=True,
+        verbose_name='Update Date',
+
     )
     create_date = models.DateTimeField(
         blank=True,
         auto_now_add=True,
+        verbose_name='Create Date',
     )
+
+    def __str__(self):
+        return f'General Setting: {self.name}'
+
+    class Meta:
+        verbose_name = 'General Setting'
+        verbose_name_plural = 'General Settings'
+        ordering = ('name', )
+
